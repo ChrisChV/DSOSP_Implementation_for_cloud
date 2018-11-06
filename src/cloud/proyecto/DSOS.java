78d2354211b68c136d643c8f37d5884a63ce29d0
ChrisChV <cristo2014f@outlook.es>
2018-11-06 01:15:22 -0500

    first commit

---
+++ src/cloud/proyecto/DSOS.java
@@ -0,0 +1,15 @@
+package cloud.proyecto;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DSOS{
+    
+    public void execute(List<Integer> taskLengs, List<Integer> vmCost, int numOrg, int iterations){
+        
+    }
+
+}
